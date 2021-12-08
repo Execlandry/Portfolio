ccda7d8f20f9fb0d0768473c63d83e50e620ab99
Execlandry <nikhilkerkarnk21@gmail.com>
2021-12-08 14:58:54 +0530

    yes

--- src/Components/body/admin/Admin.js
+++ src/Components/body/admin/Admin.js
@@ -1,13 +1,17 @@
 import React from 'react'
+import Separator from '../../common/separator/Separator'
 import './admin.css'
+import Signup from '../admin/Pages/signup/Signup'
 const Admin = () => {
     return (
         <div className="admin">
-         <div className="admin-container">
+            <Separator />
 
-        <h1>Admin page</h1>
-         </div>
-            
+            <label htmlFor="" className="section-title">
+                Admin</label>
+            <div className="admin-section-body">
+                <Signup/>
+            </div>
         </div>
     )
 }
--- src/Components/body/admin/Pages/signup/Signup.js
+++ src/Components/body/admin/Pages/signup/Signup.js
@@ -1,9 +1,73 @@
 import React from 'react';
 import './signup.css';
+import Signuppic from '../../../../../assets/Signuppic.png'
+import {NavLink} from 'react-router-dom'
 const Signup = () => {
     return (
-        <div>
-            
+        <div className="signup">
+            <div className="signup-container">
+
+                <div className="signup-section">
+                    <label className="signup-section-title">Signup</label>
+                    <div className="signup-section-form">
+                        <form className="signup-reg-form" id="signup-reg-form">
+
+                            <div className="form-group">
+                                <label htmlFor="name"><i class="user outline icon"></i></label>
+                                <input type="text" name="name" id="name" autoComplete="off"
+                                    placeholder="Enter your Name" />
+                            </div>
+
+
+                            <div className="form-group">
+                                <label htmlFor="email"> <i class="address card outline icon"></i> </label>
+                                <input type="email" name="email" id="email" autoComplete="off"
+                                    placeholder="Enter your Email" />
+                            </div>
+
+
+                            <div className="form-group">
+                                <label htmlFor="phone"><i class="phone icon"></i></label>
+                                <input type="number" name="phone" id="phone" autoComplete="off"
+                                    placeholder="Enter your Phone Number" />
+                            </div>
+
+
+                            <div className="form-group">
+                                <label htmlFor="work"><i class="briefcase icon"></i></label>
+                                <input type="text" name="work" id="work" autoComplete="off"
+                                    placeholder="Enter your Role" />
+                            </div>
+
+
+                            <div className="form-group">
+                                <label htmlFor="password"><i class="key icon"></i></label>
+                                <input type="password" name="password" id="password" autoComplete="off"
+                                    placeholder="Enter your Password" />
+                            </div>
+
+                            <div className="form-group">
+                                <label htmlFor="cpassword"><i class="key icon"></i></label>
+                                <input type="password" name="cpassword" id="cpassword" autoComplete="off"
+                                    placeholder="Confirm your Password" />
+                            </div>
+
+                                <div className="form-group">
+                                    <input className="form-submit" type="submit" name="signup" id="signup" value="Register" />
+                                </div>
+                        </form>
+
+                        <div className="signup-image">
+                            <figure>
+                                <img src={Signuppic} alt="registration" />
+                            </figure>
+                            <NavLink to="/signin" className="signup-image-link">I am already Registered</NavLink>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+
         </div>
     )
 }
