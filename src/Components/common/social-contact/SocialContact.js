c6e96d97dd9ba1f1ed0b38f9c88becd71cd11f17
Execlandry <nikhilkerkarnk21@gmail.com>
2022-01-03 12:15:19 +0530

    added functionality to icons

--- src/Components/common/social-contact/SocialContact.js
+++ src/Components/common/social-contact/SocialContact.js
@@ -1,21 +1,25 @@
 import React from 'react'
 import './social-contact.css'
-import {SocialData} from '../../data/Social'
+import { SocialData } from '../../data/Social'
 function SocialContact() {
     const data = SocialData;
+    const handleLink = () => {
+        window.open(data.item.link);
+
+    }
     return (
 
         <div className="social-contact">
-        {data.map(item=>{
-            return(
-                <a href={item.link} target="_blank">
-                    <div className="social-icon-div">
-                    <img src={item.icon} alt="social media icons" className="social-icon" />
-                    </div>
-                </a>
-            )
-        })}
-            
+            {data.map(item => {
+                return (
+                    <a href={item.link} target="_blank" rel="noreferrer">
+                        <div className="social-icon-div">
+                            <img src={item.icon} onClick={handleLink} alt="social media icons" className="social-icon" />
+                        </div>
+                    </a>
+                )
+            })}
+
         </div>
     )
 }
