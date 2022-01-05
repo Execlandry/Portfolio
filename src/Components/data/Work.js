e92d5134e12c86dfe799557f5ba44c214924d916
Execlandry <nikhilkerkarnk21@gmail.com>
2022-01-05 10:32:50 +0530

    work field

--- src/Components/data/Work.js
+++ src/Components/data/Work.js
@@ -5,6 +5,14 @@ export const WorkData=[
         dateJoining:"18/10/21",
         dateEnd:"13/12/21",
         companyLogo: "https://media.glassdoor.com/sqll/995307/fabcoders-squarelogo-1584346738940.png",
-        work:"Learn't ReactJs"
+        work:"Learn't ReactJs."
+    },
+    {
+        company:"Government Polytechnic",
+        designation:"Intern",
+        dateJoining:"11/08/19",
+        dateEnd:"15/07/22",
+        companyLogo: "https://upload.wikimedia.org/wikipedia/commons/5/51/Govt._Polytechnic_Panaji_Emblem.jpg",
+        work:"Diploma in Computer Engineering."
     }
 ]
